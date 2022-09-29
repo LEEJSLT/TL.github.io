7f26479c3241c885be1d83437a63da87d7069014
Taoli93 <48353960+LEEJSLT@users.noreply.github.com>
2022-09-29 18:39:54 +0800

    Summary

--- productOfArrayExceptSelf.java
+++ productOfArrayExceptSelf.java
@@ -1,5 +1,26 @@
 class SolutionProductExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
+    public static int[] productExceptSelf(int[] nums) {
+
+        // cannot use division operation
+        int length = nums.length;
+        int[] result = new int[length];
+        int ri = 1; // right product
+        int le = 1; // left product
+
         
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] { 1, 2, 3, 4 };
+        int[] result = productExceptSelf(a);
+
+        for (int x : result){
+            System.out.println(x);
+        }
+
+
     }
 }
