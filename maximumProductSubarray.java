36a26ec7046b93b01b9f56f2ed9cbcc9925387f7
Taoli93 <48353960+LEEJSLT@users.noreply.github.com>
2022-10-07 18:45:46 +0800

    Create maximumProductSubarray.java

---
+++ maximumProductSubarray.java
@@ -0,0 +1,15 @@
+class SolutionMaxProduct {
+    public int maxProduct(int[] nums) {
+        int ans = nums[0];
+        int product = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+
+            
+        }
+
+
+        return ans;
+        
+    }
+}
