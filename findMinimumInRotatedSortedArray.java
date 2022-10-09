a05638be5da0225c24d0f63f8e0ba336ccf05da3
Taoli93 <48353960+LEEJSLT@users.noreply.github.com>
2022-10-09 11:11:32 +0800

    Update

--- findMinimumInRotatedSortedArray.java
+++ findMinimumInRotatedSortedArray.java
@@ -1,9 +1,26 @@
 class SolutionFindMin {
-    public int findMin(int[] nums) {
+    public static int findMin(int[] nums) {
 
-        int ans = nums[0];
+        // int ans = nums[0];
+        int left = 0;   // get the left initial value
+        int right = nums.length - 1;    // get the right initial value
 
-        return ans;
+        while (left < right){
+            int mid = (left + right) / 2; // get the middle index
+            
+            if (nums[left] > nums[mid]){
+                right = mid; 
+            } else {
+                left = mid;
+            }
+        }
+
+        return nums[right];
         
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] { 3,4,5,1,2 };
+        System.out.println(findMin(a));
     }
 }
--- maximumSubarray.md
+++ maximumSubarray.md
@@ -4,6 +4,8 @@
 > Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 > A subarray is a contiguous part of an array.
 
+[LeetCode] (https://leetcode.cn/problems/maximum-subarray/)
+
 ### Solution 1 - Java - Dynamic Programming
 ```java
     public static int maxSubArray(int[] nums) {
@@ -13,6 +15,7 @@
         for (int i = 1; i < nums.length; i++){
 
             sum = Integer.max(sum+nums[i], nums[i]);
+            
             if (sum > ans){
                 ans = sum;
             }
@@ -20,5 +23,4 @@
         }
         return ans;
     }
-    
 ```
